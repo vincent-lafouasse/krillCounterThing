6cd36f232d7bcd65c0760429f8f17bde5d6a635e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-11-18 01:03:02 +0100

    simplify error handling

--- main.cpp
+++ main.cpp
@@ -9,15 +9,24 @@ char in[256];
 int out[16] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
 
 int main(int argc, char **argv) {
-	if (argc == 2) {
-		std::ifstream file(argv[1]);
-		if (file.is_open()) {
-				std::getline(file,input);
-				std::strncpy(in, input.c_str(), sizeof(in));
-				in[255] = '\0';
-		}
-	} else if (argc < 2) { printf("too few args!"); return 1;}
-	else if (argc > 2) { printf("too many args!"); return 1; }
+	if (argc < 2) {
+		printf("too few args!\n");
+		exit(1);
+	} else if (argc > 2) {
+		printf("too many args!\n");
+		exit(1);
+	}
+
+	std::ifstream file(argv[1]);
+	if (!file.is_open()) {
+		printf("failed to open file %s\n", argv[1]);
+		exit(1);
+	}
+
+	std::getline(file,input);
+	std::strncpy(in, input.c_str(), sizeof(in));
+	in[255] = '\0';
+
 	int i = 0;
 	int instruction = 0;
 	while(i <= 15 & i > -1) {
